802f8928149f5040de40d8df035b3651e30830a8
Максим <maxim@amicus.ru>
2019-06-21 09:09:13 +0300

    update js scroll animation

--- js libs/scroll-animation.js
+++ js libs/scroll-animation.js
@@ -6,31 +6,50 @@ function scrollAnimation(animatedEls) {
     animatedEls.css('visibility', 'hidden');
     var hideEls = animatedEls;
     var animationNameDefault = 'fadeIn';
-    var delay = 100;
+    var delay = 300;
+    var timeout = false;
 
     $(window).scroll(function() {
-        hideEls = hideEls.filter(function () {
-            return $(this).css('visibility') == 'hidden';
-        });
+        if (timeout !== false) {
+            clearTimeout(timeout);
+        }
 
-        var scroll = $(window).scrollTop();
-        var wHeight = $(window).height();
-        var showEls = hideEls.filter(function () {
-            var top = $(this).offset().top;
-            var height = $(this).height();
-            return scroll + wHeight > top + (height * 0.6)
-        });
-        showEls.sort(sortElements).each(function (index) {
-            let animationName = $(this).data('animation')?$(this).data('animation'):animationNameDefault;
-            delay = $(this).data('delay')?$(this).data('delay'):delay;
-            $(this).css('visibility', 'visible');
-            $(this).css('animation-delay', delay*index+'ms');
-            $(this).addClass('animated '+ animationName);
-        })
+        timeout = setTimeout(function() {
+            hideEls = hideEls.filter(function () {
+                return $(this).css('visibility') == 'hidden';
+            });
+
+            var scroll = $(window).scrollTop();
+            var wHeight = $(window).height();
+            var showEls = hideEls.filter(function () {
+                let res = false;
+                var top = $(this).offset().top;
+                var height = $(this).height();
+                if (
+                    scroll + wHeight > top + (height * 0.4)
+                    && scroll < top + (height * 0.4)
+                ){
+                    res = true;
+                }
+                return res
+            });
+
+            showEls.sort(sortElements).each(function (index) {
+                let animationName = $(this).data('animation')?$(this).data('animation'):animationNameDefault;
+                delay = $(this).data('delay')?$(this).data('delay'):delay;
+                $(this).css('visibility', 'visible');
+                $(this).css('animation-delay', delay*index+'ms');
+                $(this).addClass('animated '+ animationName);
+            })
+        }, delay);
+
+
     }).scroll();
 }
 
 function sortElements(a,b) {
+    a = $(a).index();
+    b = $(b).index();
     if (a > b) return 1;
     if (a < b) return -1;
     return 1
